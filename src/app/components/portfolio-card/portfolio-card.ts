52f0bec283d0beec7b9ffb9bf255650713c21ff1
Lucras Galindo <devlucasgalindo@gmail.com>
2026-04-17 17:12:59 -0300

    feat: scaffold portfolio-card component

---
+++ src/app/components/portfolio-card/portfolio-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-card',
+  imports: [],
+  templateUrl: './portfolio-card.html',
+  styleUrl: './portfolio-card.scss',
+})
+export class PortfolioCard {}
